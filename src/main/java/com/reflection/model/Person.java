e344edf0c0406fa49d11489aac29e8d3d179971a
Mesut Yakut <mst@mesutyakut.com>
2016-07-27 00:21:23 +0300

    Model has been added

---
+++ src/main/java/com/reflection/model/Person.java
@@ -0,0 +1,115 @@
+package com.reflection.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ * Created by mesut on 27.07.2016.
+ */
+@Entity
+public class Person extends BaseModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Size(min = 2, max = 30)
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Size(min = 2, max = 30)
+    @Column(name = "SURNAME", nullable = false)
+    private String surname;
+
+    @Column(name = "AGE", nullable = false)
+    private int age;
+
+    @Size(min = 3, max = 250)
+    @Column(name = "ADDRESS", nullable = true)
+    private String address;
+
+    @Size(min = 2, max = 40)
+    @Column(name = "CITY", nullable = true)
+    private String city;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return id.equals(person.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
