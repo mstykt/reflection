f35ed8538e054ebaa1cccbe118afe4c043693d3e
Mesut Yakut <mst@mesutyakut.com>
2016-07-26 22:54:31 +0300

    inilitialize project

---
+++ src/main/java/com/reflection/AppMain.java
@@ -0,0 +1,10 @@
+package com.reflection;
+
+/**
+ * Created by mesut on 26.07.2016.
+ */
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
